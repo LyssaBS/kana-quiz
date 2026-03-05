2e22ab164155b5d628a687aeb7a0193d6cceec87
LyssaBS <lucreziacaso.p@gmail.com>
2026-03-05 16:59:27 +0100

    jsons

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -8,18 +8,18 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   loadVocab(): Promise<VocabItem[]> {
-    return firstValueFrom(this.http.get<VocabItem[]>('/assets/data/vocab.json'));
+    return firstValueFrom(this.http.get<VocabItem[]>('assets/data/vocab.json'));
   }
 
   loadKana(): Promise<KanaItem[]> {
-    return firstValueFrom(this.http.get<KanaItem[]>('/assets/data/kana.json'));
+    return firstValueFrom(this.http.get<KanaItem[]>('assets/data/kana.json'));
   }
 
   loadVerbs(): Promise<VerbItem[]> {
-    return firstValueFrom(this.http.get<VerbItem[]>('/assets/data/verbs.json'));
+    return firstValueFrom(this.http.get<VerbItem[]>('assets/data/verbs.json'));
   }
 
   loadAdjectives(): Promise<AdjectiveItem[]> {
-    return firstValueFrom(this.http.get<AdjectiveItem[]>('/assets/data/adjectives.json'));
+    return firstValueFrom(this.http.get<AdjectiveItem[]>('assets/data/adjectives.json'));
   }
 }
